6d99bbc6412aaad54f79d95f4e3b3a814903209f
nikolasleon28 <nldo1@mscloud.ua.es>
2026-03-20 12:32:47 +0100

    Cambio ruta 2

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 function HomePage() {
   const [message, setMessage] = useState("Cargando...");
 
   useEffect(() => {
-    fetch("http://localhost:5000")
+    fetch(`${API_URL}`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch(() => setMessage("No se pudo conectar con el backend"));
